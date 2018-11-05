d69643e89102ecbcd2b80d578abc1ef3c3af3d36
wangchong666 <w420050757@gmail.com>
2018-11-05 12:53:06 +0800

    Update install.R

--- install.R
+++ install.R
@@ -2,7 +2,7 @@ install.packages('Rserve',,'http://www.rforge.net/')
 install.packages('repmis')
 require(repmis)
 Names <- c("data.table", "dplyr", "lubridate", "stringr", "forecast", "tidyr", "RJDBC")
-Vers <- c("1.11.8", "0.7.7", "1.5.0", "1.0.0", "8.3", "0.4.1", "0.2-4")
+Vers <- c("1.11.8", "0.7.7", "1.5.0", "1.0.0", "8.3", "0.4.1", "0.2.7.1")
 
 # Install old package versions into the default library
 InstallOldPackages(pkgs = Names, versions = Vers)
